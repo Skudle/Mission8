a68663d63db58daff6d6249e7126c7166488aa77
Skudle <113626842+Skudle@users.noreply.github.com>
2022-11-19 13:19:42 +0100

    Add files via upload

--- mission8.py
+++ mission8.py
@@ -1,7 +1,7 @@
 import datetime
 from copy import copy
 class Duree :
-    def __init__(self, h, m, s):
+    def __init__(self, h: int, m: int, s: int):
         """
         @pre: h, m et s sont des entiers positifs (ou zéro)
               m et s sont < 60
@@ -10,6 +10,7 @@ class Duree :
 
         if m > 59 or m < 0 or s > 59 or s < 0 or h < 0:
             raise ValueError
+
         else:
             self.hour = h
             self.minute = m
@@ -45,6 +46,7 @@ class Duree :
         @pre:  d est une instance de la classe Duree
         @post: Retourne True si cette durée (self) est plus grand que la durée
                d passée en paramètre; retourne False sinon.
+        returns: True if self is > than d. Else, returns False
         """
         return self.to_secondes() > d.to_secondes()
 
@@ -74,9 +76,12 @@ class Duree :
 
 class Chanson :
     def __init__(self, t: str, a: str, d):
-        self.titre = t.replace(" ", "_")
-        self.auteur = a.replace(" ", "_")
-        self.duree = d
+        if isinstance(t, str) and isinstance(a, str):
+            self.titre = t.replace(" ", "_")
+            self.auteur = a.replace(" ", "_")
+            self.duree = d
+        else:
+            raise TypeError
     def __str__(self):
         """
            @pre:  -
@@ -94,6 +99,13 @@ class Album :
         self.time_limit = Duree(1 ,15, 0)
 
     def add(self, chanson):
+        '''
+        args: an object from the class 'Chanson'
+        This function add a song to a list belonging to an object from class 'Album' and also add the duration of the song
+        to the album total duration
+        return: False if the album list contains 100 songs or the album total duration is longer than 1h15 min.
+        Else, it returns True
+        '''
         d = copy(self.duration)
         if (len(self.song_list)) == 100 or d.ajouter(chanson.duree).apres(self.time_limit):
             return False
@@ -103,9 +115,16 @@ class Album :
             return True
 
     def __str__(self):
-        return f"Album {self.num} {len(self.song_list)} chansons, {self.duration}\n" + "\n".join([str(e) for e in self.song_list])
+        return f"Album {str(self.num)} {str(len(self.song_list))} chansons, {str(self.duration)}\n" + "\n".join([str(e) for e in self.song_list])
+a1 = Album(1)
+print(a1)
+x = a1.__str__()
+c = x.split()
+print(c)
+print(type(c[0]))
 
-if __name__ == "__main__":
+
+'''if __name__ == "__main__":
     # Grâce à la ligne ci-dessus, le code ci-dessous ne sera exécuté que si on n'exécute ce fichier directement.
     # Ceci nous permet d'éviter que le code ci-dessous sera exécuté lorsqu'on fait un import de ce fichier,
     # par exemple dans notre fichier test.py
@@ -121,4 +140,4 @@ if __name__ == "__main__":
                 album_list[-1].add(song)
 
         for i in range(len(album_list)):
-            print(f"{album_list[i]}\n")
+            print(f"{album_list[i]}\n")'''
--- test.py
+++ test.py
@@ -16,38 +16,56 @@ from mission8 import Duree, Chanson, Album
 # CREATION DE QUELQUES OBJETS DE LA CLASSE Duree A TESTER
 d0 = Duree(0,0,0)
 d1 = Duree(10,20,59)
-d2 = Duree( 8,41,25)
-print(d0)
-
-
-
+d2 = Duree(8,41,25)
+d4 = Duree(0, 0, 0)
 # FONCTION POUT TESTER LA METHODE __str__ DE LA CLASSE Duree
 def test_Duree_str() :
     assert d1.__str__() == "10:20:59", "Test 1 Duree __str__"
     assert d2.__str__() == "08:41:25", "Test 2 Duree __str__"
-    # A COMPLETER EVENTUELLEMENT PAR LES ETUDIANTS
     
 # FONCTION POUR TESTER LA METHODE toSecondes DE LA CLASSE Duree
 def test_Duree_to_secondes() :
     assert d1.to_secondes() == 37259, "Test 1 Duree toSecondes"
     assert d2.to_secondes() == 31285, "Test 2 Duree toSecondes"
-    # A COMPLETER EVENTUELLEMENT PAR LES ETUDIANTS
+    assert d4.to_secondes() == 0, "Test 3 Duree toSecondes"
 
 # FONCTION POUR TESTER LA METHODE delta DE LA CLASSE Duree
 def test_Duree_delta():
-    # A COMPLETER PAR LES ETUDIANTS
-    pass
+    assert d1.delta(d2) == 5974, "Test 1 Duree Delta"
+    assert d2.delta(d1) == -5974, "Test 2 Duree Delta"
+    assert d0.delta(d4) == 0, "Test 3 Duree Delta"
+
     
 # FONCTION POUR TESTER  LA METHODE apres DE LA CLASSE Duree
 def test_Duree_apres():
     assert d1.apres(d2),     "Test 1 Duree apres"
     assert not d0.apres(d1), "Test 2 Duree apres"
-    # A COMPLETER PAR LES ETUDIANTS
+    assert not d0.apres(d4),  "Test 3 Duree apres"
     
 # FONCTION POUR TESTER LA METHODE ajouter DE LA CLASSE Duree
 def test_Duree_ajouter():
-    # A COMPLETER PAR LES ETUDIANTS
-    pass
+    assert d0.ajouter(d1) == d0, "Test 1 Duree ajouter"
+    assert d0.ajouter(d4) == d4 or d0, "Test 2 Duree ajouter"
+
+def test_error_init():
+    try:
+        d5 = Duree(-10, -9, -8)
+        print("Constructor function doesn't throw an error when args are neg")
+    except Exception as e:
+        if isinstance(e, ValueError):
+            print("First test error passed")
+        else:
+            print(f"Your function doesn't throw the asked exception: {repr(e)} instead of ValueError")
+
+    try:
+        d5 = Duree(61, 62, 62)
+        print("Constructor function doesn't throw an error when args are neg")
+    except Exception as e:
+        if isinstance(e, ValueError):
+            print("Second test error passed")
+        else:
+            print(f"Your function doesn't throw the asked exception: {repr(e)} instead of ValueError. Your object {d5} is must be higher than higher or equal to 0 or lower or equal to 59")
+
 
 # APPEL DES DIFFERENTES FONCTIONS TEST
 test_Duree_str()
@@ -55,7 +73,7 @@ test_Duree_to_secondes()
 test_Duree_delta()
 test_Duree_apres()
 test_Duree_ajouter()
-
+test_error_init()
 ################################
 # Tests pour la classe Chanson #
 ################################
@@ -63,11 +81,13 @@ test_Duree_ajouter()
 # CREATION DE QUELQUES OBJETS DE LA CLASSE Chanson A TESTER
 c1 = Chanson("Let's Dance", "David Bowie", Duree(0,4,5))
 c2 = Chanson("Je suis un sniper", "Locklear", Duree(0,1,38))
-tom = Album(1)
-tom.add("Let's_Dance David_Bowie 4 5")
-tom.add("Enjoy_The_Silence Depeche_Mode 4 13")
-#print(tom)
 
+def test_chanson_error_init():
+    try:
+        c3 = Chanson(88, 4, Duree(0, 2, 4))
+        print("test failed")
+    except TypeError:
+        print("Third test error passed")
 
 # FONCTION POUR TESTER LA METHODE __str__ DE LA CLASSE Chanson
 def test_Chanson_str(chanson) :
@@ -75,12 +95,18 @@ def test_Chanson_str(chanson) :
     assert c2.__str__() == "Je_suis_un_sniper - Locklear - 00:01:38", "Test 2 Chanson __str__"
 
 
+
 # APPEL DES DIFFERENTES FONCTIONS TEST
 test_Chanson_str(c1)
+test_chanson_error_init()
 
 ##############################
 # Tests pour la classe Album #
 ##############################
+a1 = Album(1)
+def test_album_str():
+    assert a1.__str__() == "Album 1 0 chansons, 00:00:00"
+
 
 # CREATION D'UN OBJET DE LA CLASSE Album A TESTER
 # à fournir par les étudiants
@@ -92,7 +118,7 @@ test_Chanson_str(c1)
 # à fournir par les étudiants
 
 # APPEL DES DIFFERENTES FONCTIONS TEST
-# à fournir par les étudiants
+test_album_str()
 
 #####################################
 # Test du comportement du programme #
